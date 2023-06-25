228170d0019c2a22ea9604ff67a5eae60654c31a
DevDecor <fiyiziyech@gmail.com>
2023-06-25 18:23:19 +0100

    Add create file puppet script

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# this manifest script creates a file if not exist
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
