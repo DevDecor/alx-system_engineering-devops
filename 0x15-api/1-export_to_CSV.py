44fb18bf4720a421dafd3e40c5ed78a3fe08de43
DevDecor <fiyiziyech@gmail.com>
2023-07-24 22:50:31 +0100

    Scrape endpoint into csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fetches data from an API endpoint"""
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    base_url = "https://jsonplaceholder.typicode.com/"
+    url = f"{base_url}todos?userId={argv[1]}&_expand=user"
+
+    res = get(url).json()
+    name = res[0]['user']['name']
+    userId = res[0]['userId']
+    with open(f"{userId}.csv", 'w', encoding="utf-8") as file:
+        for todo in res:
+            temp = f"{userId},{name},{todo['completed']},{todo['title']}\n"
+            file.write(temp)
+        file.close()
