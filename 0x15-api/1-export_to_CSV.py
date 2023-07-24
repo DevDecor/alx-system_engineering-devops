422349241efa186eced1a089fef9b1bc754937b8
DevDecor <fiyiziyech@gmail.com>
2023-07-24 22:53:28 +0100

    Scrape endpoint into csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = f"{base_url}todos?userId={argv[1]}&_expand=user"
 
     res = get(url).json()
-    name = res[0]['user']['name']
+    name = res[0]['user']['username']
     userId = res[0]['userId']
     with open(f"{userId}.csv", 'w', encoding="utf-8") as file:
         for todo in res:
