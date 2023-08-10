7d707d522a52260b5e1b2162eaefdae0123a4a73
DevDecor <fiyiziyech@gmail.com>
2023-08-10 16:30:40 +0100

    Number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
+def number_of_subscribers(subreddit) -> int:
     """Set a custom User-Agent to avoid Too Many Requests errors"""
     headers = {'User-Agent': 'MyRedditBot/1.0'}
 
@@ -11,10 +11,11 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, allow_redirects=True)
+        # response.status_code == 
         data = response.json()
 
-        if 'subscribers' in data['data']:
+        if data and 'subscribers' in data['data']:
             return data['data']['subscribers']
         else:
             return 0
